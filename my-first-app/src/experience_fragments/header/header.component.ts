35d2e080d10411295b020b78f7ab95dc923c21a2
AmitPrasad_Perifcient <amit.prasad@perficient.com>
2021-07-25 20:00:52 +0530

    working header

--- my-first-app/src/experience_fragments/header/header.component.ts
+++ my-first-app/src/experience_fragments/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
     selector: 'header-component',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+    @Output()  featureSelected = new EventEmitter<string>();
     ngOnInit() {}
+    
+    onSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 }
